36ec32e4a13f9e56aa5d9b7877eeb49d65d8b528
Arya Atighehchian <arya.atighehchian.13@cnu.edu>
2017-10-26 15:12:32 -0400

    youtubeTest file added

---
+++ src/test/java/com/coveros/appium_testing/YoutubeTest.java
@@ -0,0 +1,54 @@
+package com.coveros.appium_testing;
+
+import io.appium.java_client.android.AndroidDriver;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class YoutubeTest {
+	private static AndroidDriver<WebElement> driver;
+	
+	@BeforeClass
+	public void setup() throws MalformedURLException, InterruptedException {
+
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+		capabilities.setCapability("deviceName", "emulator");
+		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability("appPackage", "com.google.android.youtube");
+		capabilities.setCapability("appActivity", "com.google.android.apps.youtube.app.WatchWhileActivity");
+
+		driver = new AndroidDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		//driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	@Test
+	public void youtubeSearch() {
+		
+		String app_package_name = "com.google.android.youtube:";
+		driver.findElementByAccessibilityId("Search").click();
+		driver.findElementById(app_package_name+ "id/search_edit_text").sendKeys("travis scott");
+		//driver.findElementById(app_package_name+ "id/search_edit_text").sendKeys(Keys.ENTER);
+		driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.TextView").click();
+		
+		Assert.assertTrue(true);
+	}
+
+}
