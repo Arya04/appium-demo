77f718ed717283cc06a1ebfb7f569d3eed5f632b
Arya Atighehchian <arya.atighehchian.13@cnu.edu>
2017-10-26 13:23:24 -0400

    youtube search method

--- src/test/java/com/coveros/appium_testing/StartApplication.java
+++ src/test/java/com/coveros/appium_testing/StartApplication.java
@@ -5,27 +5,44 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
 
 public class StartApplication {
-	private static AndroidDriver driver;
+	private static AndroidDriver<WebElement> driver;
 
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 
-		
-
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+		capabilities.setCapability("deviceName", "emulator");
 		capabilities.setCapability("platformName", "Android");
 		capabilities.setCapability("appPackage", "com.google.android.youtube");
 		capabilities.setCapability("appActivity", "com.google.android.apps.youtube.app.WatchWhileActivity");
 
-		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-		driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+		driver = new AndroidDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		//driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+		youtubeSearch();
 		Thread.sleep(10000);
 		driver.quit();
 
+	}
+
+
+	public static void youtubeSearch() {
+		
+		String app_package_name = "com.google.android.youtube:";
+		driver.findElementByAccessibilityId("Search").click();
+		driver.findElementById(app_package_name+ "id/search_edit_text").sendKeys("travis scott");
+		//driver.findElementById(app_package_name+ "id/search_edit_text").sendKeys(Keys.ENTER);
+		driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.TextView").click();
+		
+		
 	}
 
 }
