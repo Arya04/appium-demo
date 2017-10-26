4bfae9f83934125d1bf73824e2ba40869ca8ecde
Arya Atighehchian <arya.atighehchian.13@cnu.edu>
2017-10-26 16:52:13 -0400

    YoutubeTests runs correctly

--- src/test/java/com/coveros/appium_testing/YoutubeTests.java
+++ src/test/java/com/coveros/appium_testing/YoutubeTests.java
@@ -11,12 +11,16 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class StartApplication {
+public class YoutubeTests {
 	private static AndroidDriver<WebElement> driver;
-
-	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+	
+	@BeforeClass
+	public void setup() throws MalformedURLException, InterruptedException {
 
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
@@ -27,14 +31,16 @@ public class StartApplication {
 
 		driver = new AndroidDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 		//driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
-		youtubeSearch();
-		Thread.sleep(10000);
-		driver.quit();
 
 	}
 
-
-	public static void youtubeSearch() {
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	@Test
+	public void youtubeSearch() {
 		
 		String app_package_name = "com.google.android.youtube:";
 		driver.findElementByAccessibilityId("Search").click();
@@ -42,6 +48,7 @@ public class StartApplication {
 		//driver.findElementById(app_package_name+ "id/search_edit_text").sendKeys(Keys.ENTER);
 		driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.TextView").click();
 		
+		Assert.assertTrue(true);
 	}
 
 }
