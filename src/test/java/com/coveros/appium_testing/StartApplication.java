e0b9fe16c0c06681c56f5317906e85f00ed09df2
Arya Atighehchian <arya.atighehchian.13@cnu.edu>
2017-10-26 11:50:40 -0400

    initial commit

---
+++ src/test/java/com/coveros/appium_testing/StartApplication.java
@@ -0,0 +1,31 @@
+package com.coveros.appium_testing;
+
+import io.appium.java_client.android.AndroidDriver;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class StartApplication {
+	private static AndroidDriver driver;
+
+	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+
+		
+
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability("appPackage", "com.google.android.youtube");
+		capabilities.setCapability("appActivity", "com.google.android.apps.youtube.app.WatchWhileActivity");
+
+		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+		Thread.sleep(10000);
+		driver.quit();
+
+	}
+
+}
