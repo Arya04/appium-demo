bbfa1b5829b61a1b38be31640debe5d9fa2921eb
Arya Atighehchian <arya.atighehchian.13@cnu.edu>
2017-10-26 15:08:38 -0400

    deleted AppTest

--- src/test/java/com/coveros/appium_testing/StartApplication.java
+++ src/test/java/com/coveros/appium_testing/StartApplication.java
@@ -42,7 +42,6 @@ public class StartApplication {
 		//driver.findElementById(app_package_name+ "id/search_edit_text").sendKeys(Keys.ENTER);
 		driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.TextView").click();
 		
-		
 	}
 
 }
